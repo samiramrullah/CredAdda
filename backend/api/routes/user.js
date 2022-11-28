d0b1ab0e19422db06b6c295db67b7dd9d1980172
samiramrullah <samiramrullah@gmail.com>
2022-11-28 19:47:26 +0530

    with register url

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -1,9 +1,24 @@
-const express=require('express')
-const router=express.Router();
+const express = require('express')
+const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    res.send({
-        message:"Connected"
-    })
+router.post('/register', (req, res, next) => {
+    try {
+        const name = req.body.name
+        const email = req.body.email;
+        const phNumber = req.body.phoneNumber;
+        const data = {
+            name,
+            email,
+            phNumber,
+        }
+        res.status(201).json({
+            message: "User Created",
+            data
+        })
+    } catch (error) {
+        res.status(206).json({
+            message: "Eiter no content or no proper naming"
+        })
+    }
 })
-module.exports=router;
+module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express=require('express')
 const app=express();
+const morgan=require('morgan');
 const bodyParser=require('body-parser')
 
 
-//Routes
-const userRoutes=require('./api/routes/user')
+//Routes import
+const userRoutes=require('./api/routes/user');
 
+
+//Body Parser
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//Routes used
 app.use('/user',userRoutes)
 
 module.exports=app;
