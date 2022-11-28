9dd46c8954188f2bac873f23d7ab3c52bb71e0d6
samiramrullah <samiramrullah@gmail.com>
2022-11-28 20:00:26 +0530

    with mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,26 @@
-const express=require('express')
-const app=express();
-const morgan=require('morgan');
-const bodyParser=require('body-parser')
+const express = require('express')
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const mongoose=require('mongoose')
 
 
 //Routes import
-const userRoutes=require('./api/routes/user');
+const userRoutes = require('./api/routes/user');
 
+//Mongodb connection
+const db='mongodb://localhost:27017/CredAdda';
+mongoose.connect(db).then(() => {
+    console.log('connection successful');
+}).catch((err) => console.log('Error'))
+mongoose.Promise = global.Promise;
 
 //Body Parser
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Routes used
-app.use('/user',userRoutes)
+app.use('/user', userRoutes)
 
-module.exports=app;
+module.exports = app;
---
+++ backend/models/userSchema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    name: { type: String },
+    email: { type: String },
+    phNumber: { type: String },
+})
+module.exports = mongoose.model('Users', userSchema);
