517aa892023d4f95951128ec2e3b38908a56cbd9
samiramrullah <samiramrullah@gmail.com>
2022-11-29 02:10:42 +0530

    with auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,16 @@ import Navbar from './components/Navbar'
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { Routes, Route } from 'react-router-dom'
+import Auth from './Auth/Auth';
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path='/' element={<Hero />} />
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/auth/:id' element={<Auth />} />
       </Routes>
     </div>
   )
