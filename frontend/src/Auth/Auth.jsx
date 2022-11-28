545d2ee8f44f585eecf5e523c454735e2c6ed96a
samiramrullah <samiramrullah@gmail.com>
2022-11-29 02:32:07 +0530

    functional signup

--- frontend/src/Auth/Auth.jsx
+++ frontend/src/Auth/Auth.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import sideimage from '../assets/authImg.png';
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 const Auth = () => {
     const { id } = useParams();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cnfPassword, setCnfPassword] = useState('');
+
+    const signUpHandler = (e) => {
+        e.preventDefault();
+        if (cnfPassword !== password) {
+            alert("Password Not matching")
+        }
+        else {
+            const data = {
+                email,
+                password
+            }
+            console.log(data);
+            setEmail('')
+            setPassword('')
+            setCnfPassword('')
+        }
+
+    }
+
     return (
         <>
             <section className=" font-poppins">
@@ -54,7 +76,10 @@ const Auth = () => {
                                                         className="text-lg font-medium ">Email:</label>
                                                     <input type="email"
                                                         className="w-full px-4 py-3 mt-3 bg-gray-200 rounded-lg   "
-                                                        name="" placeholder="Enter your email" />
+                                                        name="" placeholder="Enter your email"
+                                                        onChange={(e) => setEmail(e.target.value)}
+                                                        value={email}
+                                                    />
                                                 </div>
                                                 <div className="mt-5">
                                                     <div>
@@ -63,7 +88,9 @@ const Auth = () => {
                                                         <div className="relative flex items-center mt-2">
                                                             <input type="password"
                                                                 className="w-full px-4 py-3 bg-gray-200 rounded-lg   "
-                                                                name="" placeholder="Enter password" />
+                                                                placeholder="Enter password"
+                                                                onChange={(e) => setPassword(e.target.value)}
+                                                            />
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                                                 className="absolute right-0 mr-3   bi bi-eye-slash"
                                                                 fill="currentColor" viewBox="0 0 16 16">
@@ -97,23 +124,32 @@ const Auth = () => {
                                 </div>
 
                             ) : (
-                                <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                                <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
                                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                                             Create and account
                                         </h1>
-                                        <form className="space-y-4 md:space-y-6" action="#">
+                                        <form className="space-y-4 md:space-y-6" onSubmit={signUpHandler}>
                                             <div>
                                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"
+                                                    onChange={(e) => setEmail(e.target.value)}
+                                                    value={email}
+                                                    required />
                                             </div>
                                             <div>
                                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    value={password}
+                                                    onChange={(e) => setPassword(e.target.value)}
+                                                    required />
                                             </div>
                                             <div>
                                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label>
-                                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    onChange={(e) => setCnfPassword(e.target.value)}
+                                                    value={cnfPassword}
+                                                    required />
                                             </div>
                                             <div className="flex items-start">
                                                 <div className="flex items-center h-5">
@@ -123,7 +159,8 @@ const Auth = () => {
                                                     <label className="font-light text-gray-500 ">I accept the <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" to={'/'}>Terms and Conditions</Link></label>
                                                 </div>
                                             </div>
-                                            <button type="submit" className="w-full text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                                            <button type="submit" className="w-full text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                                Create an account</button>
                                             <p className="text-sm font-light text-gray-500 ">
                                                 Already have an account? <Link to={`/auth/login`} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
                                             </p>
