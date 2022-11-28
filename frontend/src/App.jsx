51d18bbc42d811711a9d179e5557f0f25519eb91
samiramrullah <samiramrullah@gmail.com>
2022-11-29 03:09:45 +0530

    with dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import { Routes, Route } from 'react-router-dom'
 import Auth from './Auth/Auth';
+import Footer from './components/Footer';
+import Dashboard from './pages/Dashboard';
 const App = () => {
   return (
     <div>
@@ -14,7 +16,9 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/auth/:id' element={<Auth />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
