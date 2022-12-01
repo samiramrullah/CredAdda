79df46a3c01a5382dc318225e5b4a7c9b2cd468c
samiramrullah <samiramrullah@gmail.com>
2022-12-01 17:07:58 +0530

    button functional with img

--- frontend/src/utils/DropDown.jsx
+++ frontend/src/utils/DropDown.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Dropdown, Space,  } from 'antd';
+import { DownOutlined, } from '@ant-design/icons';
+import { Button, Dropdown, Space, } from 'antd';
 
-const handleMenuClick = (e) => {
-  // message.info('Click on menu item.');
-  console.log('click', e.key);
-};
-const items = [
-  {
-    label: '1st menu item',
-    key: '1',
-    icon: <UserOutlined />,
-  },
-  {
-    label: '2nd menu item',
-    key: '2',
-    icon: <UserOutlined />,
-  },
-  {
-    label: '3rd menu item',
-    key: '3',
-    icon: <UserOutlined />,
-  },
-];
-const menuProps = {
-  items,
-  onClick: handleMenuClick,
-};
-const DropdownCustom = () => (
-  <Space wrap>
+const DropdownCustom = ({setSendingCountry}) => {
+  const handleMenuClick = (e) => {
+    setSendingCountry(e.key)
+  };
+  const items = [
+    {
+      label: 'Canada',
+      key: '1',
+      // icon: <NA/>,
+    },
+    {
+      label: 'Qatar',
+      key: '2',
+      // icon: <QA />,
+    },
+    {
+      label: 'USA',
+      key: '3',
+      // icon: <IN />,
+    },
+  ];
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
+  return (
+    <Space wrap>
     <Dropdown menu={menuProps}>
-      <Button>
+      <Button defaultValue={'Select'}>
         <Space>
           Button
           <DownOutlined />
@@ -38,5 +38,7 @@ const DropdownCustom = () => (
       </Button>
     </Dropdown>
   </Space>
-);
-export default DropdownCustom;
+  )
+}
+
+export default DropdownCustom
--- frontend/src/utils/Postcard.jsx
+++ frontend/src/utils/Postcard.jsx
@@ -1,72 +1,27 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
-import DropdownCustom from './DropDown'
+import { useState } from 'react';
+import DropdownCustom from './DropDown';
+import { QA, CA, US } from 'country-flag-icons/react/3x2';
 const Postcard = () => {
+    const [sendingCountry, setSendingCountry] = useState('');
+    const [recivingCountry, setRecivingCountry] = useState('')
     return (
         <section class="flex mt-10 bg-gray-100 lg:h-screen font-poppins dark:bg-gray-900 ">
             <div class="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
                 <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 md:grid-cols-2">
                     <div>
                         <div class="relative w-full h-56">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1200px-Flag_of_Canada_%28Pantone%29.svg.png" alt=""
-                                class="object-cover w-full h-full " />
+                            {sendingCountry === '1' && <CA title="CANADA"
+                                class="object-cover w-full h-full " />}
+                            {sendingCountry === '2' && <QA title="QATAR"
+                                class="object-cover w-full h-full " />}
+                            {sendingCountry === '3' && <US title="USA"
+                                class="object-cover w-full h-full " />}
                             <span class="absolute top-0 left-0 px-2 py-1 mt-2 ml-2 text-xs text-white bg-blue-700">
                                 Sender</span>
                         </div>
                         <div class="p-4 bg-white dark:bg-gray-700">
-                            <div class="flex items-center justify-between mb-1">
-                                {/* <div>
-                                    <Link to={'/'}>
-                                        <h2 class="text-xl font-semibold dark:text-gray-300">Amit Robinson</h2>
-                                    </Link>
-                                </div>
-                                <div class="flex">
-                                    <a href="as" class="mr-3 text-lg font-medium text-blue-600 dark:text-gray-300">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="14" fill="currentColor"
-                                            class="bi bi-share" viewBox="0 0 16 16">
-                                            <path
-                                                d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
-                                        </svg>
-                                    </a>
-                                    <a class="text-lg font-medium text-red-600 dark:text-gray-300" href="asdf"><svg
-                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-heart" viewBox="0 0 16 16">
-                                        <path
-                                            d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                                    </svg></a>
-                                </div> */}
-                                <DropdownCustom/>
-                            </div>
-                            <div class="flex items-center mb-4">
-                                <a href="as" class="mr-1 text-blue-600 dark:text-gray-300">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor"
-                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
-                                        <path
-                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                    </svg>
-                                </a>
-                                <span class="text-sm text-gray-600 dark:text-gray-400">Gorkha,Nepal</span>
-                            </div>
-                            <h2 class="mb-4 text-lg font-medium text-gray-700 dark:text-gray-400">
-                                We will take you different places of nepal and will translate japanese to english...
-                            </h2>
-                            <div class="flex items-center justify-between ">
-                                <div class="flex items-center">
-                                    <a href="as" class="mr-2 text-blue-700 dark:text-gray-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor"
-                                            class="bi bi-calendar-day" viewBox="0 0 16 16">
-                                            <path
-                                                d="M4.684 11.523v-2.3h2.261v-.61H4.684V6.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V8.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105zm2.805-5.093c0 .238.192.425.43.425a.428.428 0 1 0 0-.855.426.426 0 0 0-.43.43zm.094 5.093h.672V7.418h-.672v4.105z" />
-                                            <path
-                                                d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                                        </svg></a>
-                                    <span class="text-sm font-medium text-gray-700 dark:text-gray-400">3/23/2022</span>
-                                </div>
-                                <a href="asdf"
-                                    class="px-3 py-2 text-xs text-gray-100 bg-blue-700 rounded hover:bg-blue-600 hover:text-gray-100">
-                                    View Details</a>
-                            </div>
+                            <DropdownCustom setSendingCountry={setSendingCountry} setRecivingCountry={setRecivingCountry} />
                         </div>
                     </div>
                     <div>
@@ -141,7 +96,7 @@ const Postcard = () => {
                             <span class="absolute top-0 left-0 px-2 py-1 mt-2 ml-2 text-xs text-white bg-blue-700">
                                 Receiver</span>
                         </div>
-                        <div class="p-4 bg-white dark:bg-gray-700">
+                        {/* <div class="p-4 bg-white dark:bg-gray-700">
                             <div class="flex items-center justify-between mb-1">
                                 <div>
                                     <a href="as">
@@ -194,7 +149,7 @@ const Postcard = () => {
                                     class="px-3 py-2 text-xs text-gray-100 bg-blue-700 rounded hover:bg-blue-600 hover:text-gray-100">
                                     View Details</a>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
